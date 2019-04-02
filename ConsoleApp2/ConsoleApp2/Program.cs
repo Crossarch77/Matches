73d23e6f845f990baf35f89853b88f17c77ec6e1
Crossarch77 <49104295+Crossarch77@users.noreply.github.com>
2019-04-02 18:32:13 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loop();
+        }
+
+        static double Switch (double s)
+        {
+            double n = 0;
+            switch (s)
+            {
+                case 0:
+                    Console.WriteLine("Are you sure about 0? It's just 0. Press any key to try again...");
+                    Console.ReadLine();
+                    Loop();
+                    break;
+                case 1:
+                    n = s * 4;
+                    return n;
+                default:
+                    if ((s % 2 == 0) && (s > 1))
+                    {
+                        n = 2 + s * 5 / 2;
+                        return n;
+                    }
+                    else if ((s % 2 == 1) && (s > 1))
+                    {
+                        n = (s+1) * 5 / 2;
+                        return n;
+                    }
+                    else if (s < 0)
+                    {
+                        Console.WriteLine("Nice joke btw... Try again, just press any key to continue.");
+                        Console.ReadLine();
+                        Loop();
+                    }
+                    break;
+            }
+            return n;
+        }
+
+        static void Loop()
+        {
+            do
+            {
+                Console.Clear();
+                double n = 0;
+                Console.Write("Enter the number of squares: ");
+                double s = double.Parse(Console.ReadLine());
+                n = Switch(s);
+                Console.WriteLine("Number of matches is: " + n);
+                Console.WriteLine("Press E to exit or any key to try again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.E);
+        }
+    }
+}
